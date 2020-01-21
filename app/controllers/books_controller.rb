e18b3fbe4ed4aa58e05f600b4d81e68228b55bf4
shutsuchi <wwww.www.wshu@gmail.com>
2020-01-21 03:16:44 +0000

    [update] bookers-B4_test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,51 @@
 class BooksController < ApplicationController
+	def top
+	end
+
+	def new
+		@book = Book.new
+	end
+
   def index
+  	@books = Book.all
+  	@book = Book.new
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+  	@books = Book.all
+  	@book = Book.new(book_params)
+ 		if @book.save
+  		redirect_to book_path(@book), notice: 'Book was successfully created.'
+  	else
+  		render action: :index
+  	end
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		redirect_to book_path(@book), notice: 'Book was successfully updated.'
+  	else
+  		render action: :edit
+  	end
+  end
+
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path, notice: 'Book was successfully deleted.'
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 end
